56b2367ad3564af545f7603df78eb6428676d7b2
Alejandro Cuello De Moya <acuellodemoya@gmail.com>
2021-12-25 16:38:14 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -10,14 +10,14 @@ import Projects from './pages/Projects';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Header></Header>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/#about" element={<About />} />
-          <Route exact path="/#projects" element={<Projects />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/projects" element={<Projects />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Footer></Footer>
     </div>
   );
