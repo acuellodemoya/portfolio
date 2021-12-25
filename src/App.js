d6c87da2f6958c5e46f244f1a065f0ab655002b1
Alejandro Cuello De Moya <acuellodemoya@gmail.com>
2021-12-25 16:31:28 -0500

    Corregida ruta principal

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header></Header>
         <Routes>
-          <Route exact path="/#" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/#about" element={<About />} />
           <Route exact path="/#projects" element={<Projects />} />
         </Routes>
